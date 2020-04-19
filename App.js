9b76d491af9288f3a20b125242ea4a6ee3f7522e
Samuel Jimenez <sjimenezre@gmail.com>
2020-04-19 16:24:59 +0200

    Connected to Gomoku server, create match button

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
   FlatList,
   Text,
   View,
+  Button,
 } from "react-native";
 
 export default App = () => {
@@ -12,27 +13,37 @@ export default App = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("https://reactnative.dev/movies.json")
-      .then((response) => response.json())
-      .then((json) => setData(json.movies))
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  });
+    if (isLoading === true) {
+      fetch("http://192.168.1.26:4242/")
+        .then((response) => response.json())
+        .then((json) => setData(json))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+    }
+  }, [isLoading]);
+
+
+  const createNewMatch = () => {
+    fetch("http://192.168.1.26:4242/match/new")
+        .then((response) => response.json())
+        .then((json) => setData(json))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+  }
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
       {isLoading ? (
-        <ActivityIndicator />
+        <Text>Gomoku home is loading...</Text>
       ) : (
-        <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <Text>
-              {item.title}, {item.releaseYear}
-            </Text>
-          )}
-        />
+        <View>
+      <Text>{data.Message}</Text>
+      <Button
+        onPress={createNewMatch}
+        title="Create new Match!"
+        color="#841584"
+      ></Button>
+      </View>
       )}
     </View>
   );
